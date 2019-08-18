a4db6fe01b74cfe949d396e377652e45d300a5f1
Leonardo Luz <39962306+Developerlight@users.noreply.github.com>
2019-08-18 19:48:47 -0300

    create Model/Controller

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const server = express(); //server recebe acesso as tratativas do express
+//  GET, POST, PUT, DELETE
+// GET    = quando queremos buscar alguma informação da nossa api
+// POST   = quando queremos criar registro ou entidade
+// PUT    = quando queremos editar
+// DELETE = deletar
+
+mongoose.connect('mongodb+srv://tindev:tindev@cluster0-uuqw8.mongodb.net/tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(express.json()); //Informando para express que vamos usar json via post
+server.use(routes);
 
 server.listen(3333); //indentificando a porta para o server
