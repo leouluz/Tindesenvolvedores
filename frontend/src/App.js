21c6d493afc4a308ace0cac7aa817528bcd519b4
Leonardo <leo.luz14@hotmail.com>
2019-08-27 20:39:01 -0300

    problema pra logar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 
 import Routes from './routes';
-
+// posso passar qualquer propriedade para um elemento e pegar na função como um parametro
 function App() {
   return (
     <Routes />
